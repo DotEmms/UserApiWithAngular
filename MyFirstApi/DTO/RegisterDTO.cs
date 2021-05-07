d6ae69a97c8b05c14ba8513ec8401b4c75504d0b
DotEmms <emma_danckaert@live.be>
2021-05-07 11:49:07 +0200

    added validation

--- MyFirstApi/DTO/RegisterDTO.cs
+++ MyFirstApi/DTO/RegisterDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace MyFirstApi.DTO
 {
     public class RegisterDTO
     {
+        //can't be empty!
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
