b332bf51d50c6906b300463471664ab1f9646424
DotEmms <emma_danckaert@live.be>
2021-05-07 10:03:53 +0200

    Added hashing method

---
+++ MyFirstApi/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    
+    public class AccountController : ControllerBase
+    {
+        private AccountService _service;
+        public AccountController(AccountService service)
+        {
+            _service = service;
+        }
+    }
+}
---
+++ MyFirstApi/Services/AccountService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApi.Services
+{
+    public class AccountService
+    {
+        private MyFirstApiContext _context;
+
+        public AccountService(MyFirstApiContext context)
+        {
+            _context = context;
+        }
+        public async Task<AppUser> RegisterAsync(string userName, string password)
+        {
+            //keep this code for future references
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                Name = userName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
