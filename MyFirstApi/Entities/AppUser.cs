4e7ffbe45249f118f2e54b3c0a59f6c63170f86f
DotEmms <emma_danckaert@live.be>
2021-05-07 09:45:35 +0200

    Added hasing and salting

--- MyFirstApi/Entities/AppUser.cs
+++ MyFirstApi/Entities/AppUser.cs
@@ -9,5 +9,7 @@ namespace MyFirstApi
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
