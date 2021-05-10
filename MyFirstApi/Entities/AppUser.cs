3bc909f052731020e6d929b15f402f2b59ffb54f
DotEmms <emma_danckaert@live.be>
2021-05-10 11:10:10 +0200

    Added more properties to AppUser

--- MyFirstApi/Entities/AppUser.cs
+++ MyFirstApi/Entities/AppUser.cs
@@ -11,5 +11,12 @@ namespace MyFirstApi
         public string Name { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastModified { get; set; } = DateTime.Now;
+
     }
 }
