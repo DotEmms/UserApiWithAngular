2df2659dbed64a14966c75e8786cdc6e9693bb25
DotEmms <emma_danckaert@live.be>
2021-05-10 11:21:37 +0200

    Added memberDTO

---
+++ MyFirstApi/DTO/MemberDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApi.DTO
+{
+    public class MemberDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
